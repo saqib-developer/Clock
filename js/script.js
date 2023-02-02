ea1ca8ef1530372c2fd2d74c1051d9313f64b519
saqibsaqib123 <79707059+saqibsaqib123@users.noreply.github.com>
2023-02-02 12:35:28 +0500

    Saqib

--- js/script.js
+++ js/script.js
@@ -14,13 +14,16 @@ setInterval(function () {
     sec.style.transform = "rotate(" + 6 * sec_no + "deg)";
 
     let min = document.getElementById('min');
-    min.style.transform = "rotate(" + 6 * min_no + "deg)";
+    // min.style.transform = "rotate(" + 6 * min_no + "deg)";
+    min.style.transform = "rotate(" + 6 * (min_no + (0.01*((sec_no * 100) / 60))) + "deg)";
+
+    // console.log("rotate(" + 6 * (min_no + (0.01*((sec_no * 100) / 60))) + "deg)");
 
     if (hour_no > 12) {
         hour_no -= 12;
     }
 
     let hour = document.getElementById('hour');
-    hour.style.transform = "rotate(" + 30 * hour_no + "deg)";
+    hour.style.transform = "rotate(" + 30 * (hour_no + (0.01*((min_no * 100) / 60))) + "deg)";
 
 }, 500);
